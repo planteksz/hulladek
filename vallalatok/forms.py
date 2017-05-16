6af8b15d191b1e4bc75c2e7b916a9c40b99d81d4
Szilvi <planteksz95@gmail.com>
2017-05-16 15:28:15 +0200

    Fixed the form-based page but it still has problems

--- vallalatok/forms.py
+++ vallalatok/forms.py
@@ -1,14 +1,41 @@
 from django import forms
-from vallalatok.models import Telepules
+from django.forms import ModelForm
+from vallalatok.models import Telepules,Vezeto,TermeloVallalat,Telephely
+
+
 
 class VezetoReg(forms.Form):
-    nev = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
-    beosztas = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    nev = forms.CharField(label="Felelős neve:", required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    beosztas = forms.CharField(label="Felelős beosztása:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
 
 class TelepulesReg(forms.Form):
-    nev = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
-    megyek = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
-    FEJER =forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
-    megye = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
-    min_iranyitoszam = forms.IntegerField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
-    max_iranyitoszam = forms.IntegerField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    nev = forms.CharField(label="Település neve:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    # megyek = forms.CharField(label="Megye:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'})
+   # FEJER =forms.CharField(label="Vezető neve",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    #megye = forms.IntegerField(label="Megye:", required=False, widget=forms.Select(choices=(enumerate(megyek))) ))
+    #min_iranyitoszam = forms.IntegerField(label="Irányítószám",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    #max_iranyitoszam = forms.IntegerField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+
+class TermeloVallalatReg(forms.Form):
+    KUJ = forms.CharField(label="KUJ szám:",required=True, max_length=9, help_text='Maximum 9 karakter!', widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    KSH = forms.CharField(label="KSH szám:",required=True, max_length=9, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    nev = forms.CharField(label="Termelőválallat neve:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    iranyitoszam = forms.IntegerField(label="Irányítószám:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    telepules = forms.CharField(label="Település:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    kozterulet_nev = forms.CharField(label="Közterület neve:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    kozterulet_tipus = forms.CharField(label="Közterület típusa:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    hazszam = forms.IntegerField(label="Házszám:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+
+class TelephelyReg(forms.Form):
+    KTJ = forms.CharField(label="KTJ szám:",required=True, max_length=9, help_text='Maximum 9 karakter!', widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    nev = forms.CharField(label="Telephely neve:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    tipus = forms.CharField(label="Telephely típusa:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    iranyitoszam = forms.IntegerField(label="Irányítószám:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    telepules = forms.CharField(label="Település:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    kozterulet_nev = forms.CharField(label="Közterület neve:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    kozterulet_tipus = forms.CharField(label="Közterület típusa:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    hazszam = forms.IntegerField(label="Házszám:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    foglalkoztatottak_szama = forms.IntegerField(label="Foglalkoztatottak száma:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    felelos_nev = forms.CharField(label="Felelős neve:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+    felelos_beosztas = forms.CharField(label="Felelős beosztása:",required=True, widget=forms.TextInput(attrs={'class': 'form-control'} ))
+
--- vallalatok/views.py
+++ vallalatok/views.py
@@ -7,6 +7,7 @@ from django.views import generic
 from django.shortcuts import render
 from vallalatok import models,forms
 from .models import Telepules, TermeloVallalat,Telephely,Tevekenyseg
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -14,12 +15,10 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return TermeloVallalat.objects.order_by('-pub_date')[:5]
 
-
 #class AdatFelView(generic.ListView):
  #   template_name = 'vallalatok/adatfel.html'
   #  def get_queryset(self):
    #     return TermeloVallalat.objects.order_by('-pub_date')[:5]
-
 
 #class NyilvView(generic.ListView):
    # model = TermeloVallalat
@@ -27,8 +26,6 @@ class IndexView(generic.ListView):
     #def get_queryset(self):
     #    """Return the last five published questions."""
      #   return TermeloVallalat.objects.order_by('-pub_date')[:5]
-
-
 
 def index(request):
     return render( request, 'index.html')
@@ -49,6 +46,9 @@ def nyilvantart(request):
 def adatfel(request):
     form_data=forms.VezetoReg(request.POST or None)
     form_data2=forms.TelepulesReg(request.POST or None)
+    form_data3 = forms.TermeloVallalatReg(request.POST or None)
+    form_data4 = forms.TelephelyReg(request.POST or None)
+  
     msg=''
     if form_data.is_valid():
         felelos = models.Vezeto()
@@ -56,26 +56,56 @@ def adatfel(request):
         felelos.beosztas = form_data.cleaned_data['beosztas']
         felelos.save()
 
-     
     if form_data2.is_valid():
-       telep = models.Telepules()
-      #  telep.nev = form_data.cleaned_data['nev']
-       # telep.megyek = form_data.cleaned_data['nev']
+        telep = models.Telepules()
+        telep.nev = form_data2.cleaned_data['nev']
+
+        #telep.megye = form_data2.cleaned_data['megye']
         #telep.FEJER = form_data.cleaned_data['nev']
-        telep.min_iranyitoszam = form_data.cleaned_data['nev']
-        telep.max_iranyitoszam = form_data.cleaned_data['nev']
+        #telep.min_iranyitoszam = form_data.cleaned_data['nev']
+        #telep.max_iranyitoszam = form_data.cleaned_data['nev']
         telep.save()
 
-    msg='Elmentettük az adatokat!'
+    if form_data3.is_valid():
+        termval = models.TermeloVallalat()
+        termval.KUJ = form_data3.cleaned_data['KUJ']
+        termval.KSH = form_data3.cleaned_data['KSH']
+        termval.nev = form_data3.cleaned_data['nev']
+        termval.iranyitoszam = form_data3.cleaned_data['iranyitoszam']
+        #termval.telepules = form_data3.cleaned_data['telepules']
+
+       # termval.telepules=form_data3.cleaned_data['telepules']
+        termval.telepules=Telepules.nev
+        termval.kozterulet_nev = form_data3.cleaned_data['kozterulet_nev']
+        termval.kozterulet_tipus = form_data3.cleaned_data['kozterulet_tipus']
+        termval.hazszam = form_data3.cleaned_data['hazszam']
+        termval.save()
+
+    if form_data4.is_valid():
+        teleph = models.Telephely()
+        teleph.KTJ = form_data4.cleaned_data['KTJ']
+        teleph.nev = form_data4.cleaned_data['nev']
+        teleph.iranyitoszam = form_data4.cleaned_data['iranyitoszam']
+        #teleph.telepules = form_data4.cleaned_data['telepules']
+        teleph.telepules=Telepules.nev
+        teleph.telepules=form_data4.cleaned_data['telepules']
+        teleph.kozterulet_nev = form_data4.cleaned_data['kozterulet_nev']
+        teleph.kozterulet_tipus = form_data4.cleaned_data['kozterulet_tipus']
+        teleph.hazszam = form_data4.cleaned_data['hazszam']
+        teleph.foglalkoztatottak_szama = form_data4.cleaned_data['foglalkoztatottak_szama']
+        teleph.felelos_nev = form_data4.cleaned_data['felelos_nev']
+        teleph.felelos_beosztas = form_data4.cleaned_data['felelos_beosztas']
+        teleph.save()
+        msg='Elmentettük az adatokat!'
     context={
-         'formregister':form_data,
-         'formregister2':form_data2,
-         'msg':msg
+        'formregister':form_data,
+        'formregister2':form_data2,
+        'formregister3':form_data3,
+        'formregister4':form_data4,
+        'msg':msg
     }
     return render( request, 'vallalatok/adatfel.html', context)
 
-#def nyilvantart(request):
-  #  return render(request, 'nyilva.html')
 
 
 
